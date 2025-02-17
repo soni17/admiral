c4617d308113639736e6622956fd975fd339a761
Enderson Isai <elb004@gmail.com>
2025-02-17 01:10:24 -0500

    add volumes tab

--- resources/js/main.js
+++ resources/js/main.js
@@ -176,3 +176,50 @@ function containersTab(el){
 	interval = setInterval(dockerContainers, 5000);
 	activeIntervals.push(interval);
 }
+
+function dockerVolumes(){
+	Neutralino.os.execCommand("docker volume ls --format json").then((res) => {
+		let images = [];
+		let str = res.stdOut.split("\n");
+
+		for (let key in str) {
+			if (str[key].length != 0) {
+				let img = JSON.parse(str[key]);
+				images.push(img);
+			}
+		}
+
+		let html = `
+		<table id="img-table">
+			<tr>
+				<th>Name</th>
+				<th>Image</th>
+				<th>Status</th>
+				<th>Size</th>
+			</tr>
+		`;
+
+		images.forEach((img) => {
+			html =
+				html +
+				`
+				<tr>
+					<td>${img["Name"].substr(0,10)}</td>
+					<td>${img["Scope"]}</td>
+					<td>${img["Status"]}</td>
+					<td>${img["Size"]}</td>
+				</tr>
+			`;
+		});
+
+		document.querySelector("#content").innerHTML = html;
+	});
+}
+
+function volumesTab(el){
+	selNav(el);
+	dockerVolumes();
+	clearIntervals();
+	interval = setInterval(dockerVolumes, 5000);
+	activeIntervals.push(interval);
+}
